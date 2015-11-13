a3bcb3d5a46895f6dc4897c22f55105857ba7316
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-11-13 14:14:01 +0100

    create Bucketlist model

--- bucketlist_django/bucketlist/models.py
+++ bucketlist_django/bucketlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Bucketlist(models.Model):
+    name = models.CharField(max_length=50)
+    date_created = models.DateField()
+    date_modified = models.DateField()
+    created_by = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
