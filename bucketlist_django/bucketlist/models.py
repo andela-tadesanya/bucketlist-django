640bf8a539ba5f6d58207089efe934a7bb75d44c
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-11-13 19:12:28 +0100

    create bucketlist items model

--- bucketlist_django/bucketlist/models.py
+++ bucketlist_django/bucketlist/models.py
@@ -3,11 +3,38 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Bucketlist(models.Model):
-    name = models.CharField(max_length=50)
-    date_created = models.DateField()
-    date_modified = models.DateField()
+    name = models.CharField(
+                            'bucketlist name',
+                             max_length=50,
+                             blank=False)
+    date_created = models.DateTimeField('created on',
+                                         auto_now_add=True)
+    date_modified = models.DateTimeField('date last modified'
+                                         auto_now=True)
     created_by = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ['date_created']
+        verbose_name = 'bucketlist'
+        verbose_name_plural = 'bucketlists'
 
     def __unicode__(self):
         return self.name
 
+class BucketlistItem(models.Model):
+    name = models.CharField('bucketlist item name', max_length=150, blank=False)
+    date_created = models.DateTimeField('created on',
+                                         auto_now_add=False)
+    date_modified = models.DateTimeField('last modified'
+                                         auto_now=False)
+    done = models.BooleanField('Done',
+                                default=False)
+    bucketlist = models.ForeignKey(Bucketlist)
+
+    class Meta:
+        ordering = [date_created]
+        verbose_name = 'bucketlist item'
+        verbose_name_plural = 'bucketlist items'
+
+    def __unicode__(self):
+        return self.name
