c684cbdb1888ec8a0238fe7135b264589b1bf834
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-11-19 10:16:37 +0100

    Change permissions to anyone, create view for bucketlist

---
+++ bucketlist_django/bucketlist/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from bucketlist import views
+
+
+urlpatterns = [
+    url(r'^bucketlist/$', views.BucketListView.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- bucketlist_django/bucketlist/views.py
+++ bucketlist_django/bucketlist/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from bucketlist.models import Bucketlist, BucketlistItem
+from bucketlist.serializers import BucketlistSerializer, BucketlistItemSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class BucketListView(APIView):
+    '''list all bucketlists'''
+    def get(self, request, format=None):
+        bucketlists = Bucketlist.objects.all()
+        serializer = BucketlistSerializer(bucketlists, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BucketlistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
