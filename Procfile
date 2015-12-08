46d3f770d6f56c84d2ab41b0e8205b72c6a86699
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-12-08 16:31:23 +0100

    create procfile and edit settings for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bucketlist_django.wsgi_production --pythonpath=bucketlist_django --log-file -
--- bucketlist_django/bucketlist_django/wsgi_production.py
+++ bucketlist_django/bucketlist_django/wsgi_production.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bucketlist_django.settings.production")
-
-application = get_wsgi_application()
