ab3163f48cfaa25130225df08224ffbf9eb5dc14
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-12-08 19:38:40 +0100

    removed reference to static at base

--- bucketlist_django/bucketlist_django/settings/defaults.py
+++ bucketlist_django/bucketlist_django/settings/defaults.py
@@ -108,7 +108,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
     'C:/Users/Andela/projects/bucketlist-django/bucketlist_django/bucketlist/static',
 )
 
