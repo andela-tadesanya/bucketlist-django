1d5be3b0f4d6281060f28aa5a160e3bd8d9d5d24
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-12-08 20:36:21 +0100

    set static file finders in settings

--- bucketlist_django/bucketlist_django/settings/defaults.py
+++ bucketlist_django/bucketlist_django/settings/defaults.py
@@ -101,15 +101,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-# Static Root
-# Static asset configuration
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    'C:/Users/Andela/projects/bucketlist-django/bucketlist_django/bucketlist/static',
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
--- bucketlist_django/bucketlist_django/settings/production.py
+++ bucketlist_django/bucketlist_django/settings/production.py
@@ -15,10 +15,16 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    'C:/Users/Andela/projects/bucketlist-django/bucketlist_django/bucketlist/static',
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
