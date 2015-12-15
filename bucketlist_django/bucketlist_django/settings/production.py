b8082ecbfbf3efcb98a7cdd5ccc7944691f13eab
oluwatosin adesanya <oluwatosin.adesanya@andela.com>
2015-12-15 13:22:54 +0100

    change static url to static

--- bucketlist_django/bucketlist_django/settings/production.py
+++ bucketlist_django/bucketlist_django/settings/production.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '..', 'bucketlist', 'static'),
